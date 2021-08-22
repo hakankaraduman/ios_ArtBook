4c6be35de8802b04f32b8ca928af7bb080bb7815
hakan <karadumanhakan1@gmail.com>
2021-08-22 19:31:09 +0300

    project is done

---
+++ ArtBook/AddViewController.swift
@@ -0,0 +1,180 @@
+//
+//  AddViewController.swift
+//  ArtBook
+//
+//  Created by Admin on 21.08.2021.
+//
+
+import UIKit
+import CoreData
+
+class AddViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var artistField: UITextField!
+    @IBOutlet weak var yearField: UITextField!
+    @IBOutlet weak var saveButton: UIButton!
+    
+    var isCustomImageSelected = false
+    var paintingId: UUID?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let viewTap = UITapGestureRecognizer(target: self, action: #selector(onViewTap))
+        view.addGestureRecognizer(viewTap)
+        
+        imageView.isUserInteractionEnabled = true;
+        let imageTap = UITapGestureRecognizer(target: self, action: #selector(onImageTap))
+        imageView.addGestureRecognizer(imageTap)
+        
+        // navigationController?.navigationBar.topItem?.rightBarButtonItems = [UIBarButtonItem(barButtonSystemItem: .save, target: self, action: #selector(onSave))]
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        print("paintingId", paintingId?.uuidString ?? "none")
+        isCustomImageSelected = false
+        saveButton.isEnabled = false
+        if paintingId != nil {
+            saveButton.isHidden = true
+            getPainting()
+        }
+    }
+    
+    func getPainting(){
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Painting")
+        request.predicate = NSPredicate(format: "id = %@", paintingId!.uuidString)
+        request.returnsObjectsAsFaults = false
+        do {
+            let results = try context.fetch(request) as! [NSManagedObject]
+            if results.count != 1 {
+                print("not found only one")
+                return
+            }
+            let result = results[0]
+            
+            let _name = result.value(forKey: "name") as? String
+            let _artist = result.value(forKey: "artist") as? String
+            let _yearInt = result.value(forKey: "year") as? Int
+            let _imageData = result.value(forKey: "image") as? Data
+            if _name != nil && _artist != nil && _yearInt != nil && _imageData != nil {
+                nameField.text = _name
+                artistField.text = _artist
+                yearField.text = String(_yearInt!)
+                imageView.image = UIImage(data: _imageData!)
+            }
+        } catch {
+            print("error on gePainting")
+        }
+        
+    }
+
+    @IBAction func onSave(_ sender: Any) {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let data = NSEntityDescription.insertNewObject(forEntityName: "Painting", into: context)
+        
+        data.setValue(UUID(), forKey: "id")
+        
+        if imageView.image == nil {
+            return
+        }
+        if let imageData = imageView.image?.jpegData(compressionQuality: 0.8) {
+            data.setValue(imageData, forKey: "image")
+        }
+        if let name = nameField.text {
+            data.setValue(name, forKey: "name")
+        } else {
+            return
+        }
+        if let artist = artistField.text {
+            data.setValue(artist, forKey: "artist")
+        } else {
+            return
+        }
+        if let yearString = yearField.text {
+            if let year = Int(yearString) {
+                data.setValue(year, forKey: "year")
+            } else {
+                return
+            }
+        } else {
+            return
+        }
+        
+        do {
+            try context.save()
+            print("success")
+        } catch {
+            print("error")
+        }
+        
+        NotificationCenter.default.post(name: NSNotification.Name.init(rawValue: "newData"), object: nil)
+        navigationController?.popViewController(animated: true)
+    }
+    
+    @objc func onImageTap(){
+        let picker = UIImagePickerController()
+        picker.allowsEditing = false
+        picker.sourceType = .photoLibrary
+        picker.delegate = self
+        present(picker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let selectedImage = info[.originalImage] as? UIImage
+        if selectedImage != nil {
+            imageView.image = selectedImage
+            isCustomImageSelected = true
+        }
+        dismiss(animated: true, completion: nil)
+        checkIfCanSave()
+    }
+    
+    @objc func onViewTap(){
+        view.endEditing(true)
+    }
+    
+    @objc func onSave(){
+        print("onSave")
+    }
+    
+    func checkIfCanSave(){
+        saveButton.isEnabled = true
+        if isCustomImageSelected == false {
+            saveButton.isEnabled = false
+        }
+        if let nameText = nameField.text {
+            if nameText.count < 1 {
+                saveButton.isEnabled = false
+            }
+        }
+        if let artistText = artistField.text {
+            if artistText.count < 1 {
+                saveButton.isEnabled = false
+            }
+        }
+        if let yearText = yearField.text {
+            if yearText.count < 4 {
+                saveButton.isEnabled = false
+            }
+        }
+    }
+    
+    @IBAction func onNameEdited(_ sender: Any) {
+        checkIfCanSave()
+    }
+    
+    @IBAction func onArtistEdited(_ sender: Any) {
+        checkIfCanSave()
+    }
+    
+    @IBAction func onYearEdited(_ sender: Any) {
+        checkIfCanSave()
+    }
+}
--- ArtBook/ViewController.swift
+++ ArtBook/ViewController.swift
@@ -6,14 +6,97 @@
 //
 
 import UIKit
+import CoreData
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var paintings = [NSManagedObject]()
+    var selectedPaintingId: UUID?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        navigationController?.navigationBar.topItem?.rightBarButtonItems = [UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(onAdd))]
+        
+        getData()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        selectedPaintingId = nil
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(getData), name: NSNotification.Name?.init(NSNotification.Name.init("newData")), object: nil)
+    }
+    
+    @objc func getData(){
+        paintings.removeAll()
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Painting")
+        request.returnsObjectsAsFaults = false
+        do {
+            let results = try context.fetch(request) as! [NSManagedObject]
+            for result in results {
+                paintings.append(result)
+            }
+        } catch {
+            print("error on getData")
+        }
+        tableView.reloadData()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return paintings.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = paintings[indexPath.row].value(forKey: "name") as? String ?? "Corrupted Data"
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        selectedPaintingId = paintings[indexPath.row].value(forKey: "id") as? UUID
+        performSegue(withIdentifier: "toAddViewController", sender: self);
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle != .delete {
+            return
+        }
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        context.delete(paintings[indexPath.row])
+        do {
+            try context.save()
+            paintings.remove(at: indexPath.row)
+            tableView.reloadData()
+        } catch {
+            print("failed to save")
+        }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier != "toAddViewController" {
+            return
+        }
+        if let addViewController = segue.destination as? AddViewController {
+            addViewController.paintingId = selectedPaintingId
+        }
     }
 
+    @objc func onAdd(){
+        performSegue(withIdentifier: "toAddViewController", sender: self);
+    }
 
 }
 
